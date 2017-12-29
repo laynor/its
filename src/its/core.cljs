007ea43bbc186738376d24c39db1ea8a9e5df261
Alessandro Piras <laynor@gmail.com>
2017-12-29 09:07:17 +0100

    wrap x11 related stuff; move modified tinywm code into core.

--- src/its/core.cljs
+++ src/its/core.cljs
@@ -1,8 +1,53 @@
 (ns ^:figwheel-always its.core
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [cljs.nodejs :as nodejs]
-            [its.tinywm :as tinywm]))
+            [clojure.core.async :as async :refer [put! <! >! close! chan timeout]]
+            [its.util :as util]
+            [its.window :as window]
+            [its.x11 :as x11]
+            [its.event-handler :refer [handle-event]]
+            [its.state :refer [state]]))
 
-(def x11 (nodejs/require "x11"))
+
+(defn its-init!
+  "WM initialization"
+  [error display]
+  (let [client display.client
+        root (window/get-screen-root client 0)
+        mod-1-mask 2r1000
+        keysym-f1 67
+        grab-mode-async 1
+        none 0]
+    (swap! state assoc :client client)
+
+    (println "mastucci")
+
+    (x11/grab-key client root true #{:meta} keysym-f1 :async :async)
+
+    (x11/grab-button client root true
+                     #{:button-press :button-release :pointer-motion}
+                     :async :async
+                     nil nil
+                     :left #{:meta})
+
+    (x11/grab-button client root true
+                     #{:button-press :button-release :pointer-motion}
+                     :async :async
+                     nil nil
+                     :right #{:meta})))
+
+
+(defn start-event-handler! [f events]
+  "Starts an asynchronous event handler. f is called on the event."
+  (go-loop [retval nil]
+    (let [ev (<! events)]
+      (when-not (= retval ::quit)
+        (recur (f (util/lispify-event ev)))))))
+
+
+(defn its []
+  (let [[client events] (x11/create-client ":1" its-init!)]
+    (start-event-handler! handle-event events)))
 
 
 (nodejs/enable-util-print!)
@@ -10,6 +55,6 @@
 
 (defn -main
   []
-  (tinywm/tinywm))
+  (its))
 
 (set! *main-cli-fn* -main) ;; this is required
--- src/its/x11.cljs
+++ src/its/x11.cljs
@@ -1,8 +1,32 @@
 (ns ^:figwheel-always its.x11
   (:require [cljs.nodejs :as nodejs]
-            [clojure.core.async :as async :refer [put! <! >! close! chan timeout]]))
+            [clojure.core.async :as async :refer [put! <! >! close! chan timeout]]
+            [its.util :as util]))
 
 (def nodex11 (nodejs/require "x11"))
+
+(def event-mask->x11
+  (util/lispify nodex11.eventMask))
+
+(def grab-mode->x11
+  {:sync 0
+   :async 1})
+
+(def mouse-button->x11
+  {:left   1
+   :middle 2
+   :right  3})
+
+(def none 0)
+
+(def modmask->x11
+  {:shift   1
+   :lock    2
+   :control 4
+   :meta    8
+   :fixme   10
+   :hyper   20
+   :super   40})
 
 (defn create-client [display-name init-fun]
   "Returns an x11 client and its event channel as [client events]"
@@ -10,3 +34,29 @@
         client (nodex11.createClient #js {:display display-name} init-fun)]
     (.on client "event" (partial put! events))
     [client events]))
+
+
+(defn- mods->x11 [mods]
+  (reduce bit-or (map modmask->x11 mods)))
+
+(defn grab-key
+  [client wid owner-events modifiers key pointer-mode keyb-mode]
+  (.GrabKey client wid
+            owner-events
+            (mods->x11 modifiers)
+            key
+            (grab-mode->x11 pointer-mode)
+            (grab-mode->x11 keyb-mode)))
+
+(defn grab-button
+  [client wid owner-events mask pointer-mode keyb-mode confine-to cursor button modifiers]
+  (.GrabButton client
+               wid
+               owner-events
+               (reduce bit-or 0 (map event-mask->x11 mask))
+               (grab-mode->x11 pointer-mode)
+               (grab-mode->x11 keyb-mode)
+               (or confine-to none)
+               (or cursor none)
+               (mouse-button->x11 button)
+               (mods->x11 modifiers)))
